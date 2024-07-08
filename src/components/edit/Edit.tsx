c91ad97d58c112b512d015389f5568037a1accd6
Abubakar-Meigag <113248042+Abubakar-Meigag@users.noreply.github.com>
2024-07-08 17:14:33 +0100

    update

--- src/components/edit/Edit.tsx
+++ src/components/edit/Edit.tsx
@@ -2,25 +2,6 @@ import React from 'react'
 
 const Edit = () => {
 
-  const fetchData = async () => {
-    const url = 'https://api.thecatapi.com/v1/images/search?limit=10&breed_ids=beng&api_key=REPLACE_ME'
-  
-    try {
-      const res = await fetch(url);
-      if ( !res.ok ) {
-        throw new Error(`res status = ${res.status}`)
-      }
-  
-      const jsonFile = await res.json();
-      console.log(jsonFile);
-    } catch (err) {
-        console.error(err)
-    }
-  }
-
-  
-  fetchData();
-
   return (
     <div>Edit component</div>
   )
